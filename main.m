2bc93aa7ca7daa4fec1c7a9965cfb9824640439c
francopestilli <frakkopesto@gmail.com>
2016-10-11 14:24:12 -0400

    ENH fixing some typos

--- main.m
+++ main.m
@@ -1,22 +1,24 @@
-
+function main
+%
+%
+%
 disp('loading application paths')
 
-%TODO - move this to more permanent location - probably even /N/soft?
+% TODO - move this to more permanent location - probably even /N/soft?
 addpath(genpath('/N/u/hayashis/BigRed2/git/encode'))
 addpath(genpath('/N/u/hayashis/BigRed2/git/vistasoft'))
 addpath(genpath('/N/u/hayashis/BigRed2/git/mba'))
-
-addpath('lib')
+addpath(genpath(fullfile(pwd,'lib')))
 
 %disp("loading demo data")
 addpath(genpath('/N/dc2/scratch/hayashis/sca/demo_data_encode'))
 
-config = loadjson('config.json')
+config = loadjson('config.json');
 disp(config)
 
 disp('running connectome_data_comparison')
-connectome_data_comparison(config)
+connectome_evaluator(config)
 
 %all sca service needs to write products.json - empty for now
-products = []
+products = [];
 savejson('', products, 'FileName', 'products.json')
