5d88e209a3fbc858a79067cf77b26e3780d8c20f
Soichi Hayashi <hayashis@iu.edu>
2016-10-16 21:47:11 -0400

    added MATLABPATH

--- start.sh
+++ start.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+#allows test execution
+if [ -z $SCA_SERVICE_DIR ]; then
+    export SCA_SERVICE_DIR=`pwd`
+fi
+if [ -z "$SCA_PROGRESS_URL" ]; then
+    export SCA_PROGRESS_URL="https://soichi7.ppa.iu.edu/api/progress/status/_sca.test"
+fi
+
 module load matlab
 
 rm -f finished
 echo "starting main.m"
 (
+export MATLABPATH=$SCA_SERVICE_DIR
 nohup time matlab -nodisplay -r main > stdout.log 2> stderr.log
 echo $? > finished
 ) &
